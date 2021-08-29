dd14ad8053b1f2a01653d0a5d5a17dc0f1efd51f
vinson <dagger1245653@gmail.com>
2021-08-29 07:47:31 -0700

    add basic display and functions to weather app

---
+++ src/index.js
@@ -0,0 +1,47 @@
+import './style.css';
+const search = document.querySelector('#search');
+const submit = document.querySelector('#submit');
+const errorMessage = document.querySelector('#errorMessage')
+submit.addEventListener('click', (event) => {
+    event.preventDefault();
+    getData(search.value);
+})
+async function getData(location) {
+    try {
+        //Jsonifys the string, and them process it
+        let data = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&APPID=68a43187485fd7018b65c705f29ea3cb`)
+        let jsonify = await data.json();
+        processJSON(jsonify);
+        console.log(jsonify)
+        errorMessage.textContent = '';
+    } catch (error) {
+        errorMessage.textContent = 'The location doesn\'t exist!';
+    }
+}
+function processJSON(json) {
+    try {
+        //create a weather object and then display it on page
+        let weather = {
+            location: json.name,
+            temperature: json.main.temp,
+            min: json.main.temp_min,
+            max: json.main.temp_max
+        }
+        errorMessage.textContent = '';
+        displayWeather(weather);
+    } catch {
+        errorMessage.textContent = 'The location doesn\'t exist!';
+    }
+}
+function displayWeather(weatherObject) {
+    let location = document.querySelector('#location');
+    let temperature = document.querySelector('#temp');
+    let min = document.querySelector('#min');
+    let max = document.querySelector('#max');
+
+    location.textContent = weatherObject.location;
+    temperature.textContent = weatherObject.temperature;
+    min.textContent = weatherObject.min;
+    max.textContent = weatherObject.max;
+}
+getData('London');
